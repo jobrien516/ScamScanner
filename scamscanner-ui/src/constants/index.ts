951288396903c2c8317c5a1665482299ea0eab86
89jobrien <98370624+89jobrien@users.noreply.github.com>
2025-08-18 12:12:09 -0400

    Update index.ts

--- scamscanner-ui/src/constants/index.ts
+++ scamscanner-ui/src/constants/index.ts
@@ -1,6 +1,4 @@
 export const BACKEND_API_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 
-export const STRIPE_PUBLIC_ID = process.env.NEXT_PUBLIC_STRIPE_ID;
-
 export * from "./pageContent";
