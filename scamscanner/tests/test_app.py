24d06d2a4432d3439a4a5949be59f7238eb5f6ef
89jobrien <98370624+89jobrien@users.noreply.github.com>
2025-08-18 12:09:05 -0400

    Update test_app.py

--- scamscanner/tests/test_app.py
+++ scamscanner/tests/test_app.py
@@ -5,9 +5,6 @@ from scamscanner.app import create_app
 
 @pytest.fixture
 def client():
-    """
-    Create a test client for the FastAPI application.
-    """
     app = create_app()
     with TestClient(app) as c:
         yield c
